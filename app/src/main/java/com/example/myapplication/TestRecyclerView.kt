c674550407f00a24259a41d3e4f8af01698486e1
Hardt, Simon <simon.hardt@loewen.de>
2018-11-09 09:38:10 +0100

    initial commit

---
+++ app/src/main/java/com/example/myapplication/TestRecyclerView.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class TestRecyclerView(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int
+) : RecyclerView(context, attrs, defStyle) { }
